5d78b5ac9bdbc3f3d382a0386237ce9c28be62a8
truong thai binh <thaibinh291201@gmail.com>
2020-08-26 11:34:50 +0700

    newcommit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello My Friends</p>
+        <p>Công Gà Noober69</p>
         <a
           className="App-link"
           href="https://reactjs.org"
